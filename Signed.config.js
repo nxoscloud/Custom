82a1d8c4b14a21e5cf140618b0b2fb15b3279845
cassitly <jedi.korn123@gmail.com>
2024-12-30 15:15:53 +0700

    Update some stuff

---
+++ Signed.config.js
@@ -0,0 +1,14 @@
+module.exports = function Information() {
+    const Information = {
+        name: 'Custom',
+        author: 'Cassitydev',
+        repo: 'https://github.com/nxoscloud/Custom',
+        description: '(Custom package for NextLanguage) A Custom testing package for NextLanguages v2.0-dev plugin system',
+        enabled: true,
+        version: '0.1.0',
+        license: null,
+        app: 'A:\\NextLanguage\\Custom\\default.js'
+    }
+    
+    return Information
+}
--- default.js
+++ default.js
@@ -1,13 +1,13 @@
-        const addOutput = require('../NextLanguage-Source/modules/functions/addOutput.js');
-        const debugOutput = require('../NextLanguage-Source/modules/functions/debugOutput.js');
+const addOutput = require('../NextLanguage-Source/modules/functions/addOutput.js');
+const debugOutput = require('../NextLanguage-Source/modules/functions/debugOutput.js');
     
-        const { getVariables, setVariable } = require('../NextLanguage-Source/modules/functions/temp/Variables.js');
-        const { getFunctions, setFunction } = require('../NextLanguage-Source/modules/functions/temp/Functions.js');
-    
-        const createPreload = require("../NextLanguage-Source/patches/v1.9/modules/nodejs/pre/createPreload.js");
-        const runPreload = require("../NextLanguage-Source/patches/v1.9/modules/nodejs/pre/runPreload.js");
-    
-        const createPostload = require("../NextLanguage-Source/patches/v1.9/modules/nodejs/post/createPostload.js");
-        const runPostload = require("../NextLanguage-Source/patches/v1.9/modules/nodejs/post/runPostload.js");
+const { getVariables, setVariable } = require('../NextLanguage-Source/modules/functions/temp/Variables.js');
+const { getFunctions, setFunction } = require('../NextLanguage-Source/modules/functions/temp/Functions.js');
+
+const createPreload = require("../NextLanguage-Source/patches/v1.9/modules/nodejs/pre/createPreload.js");
+const runPreload = require("../NextLanguage-Source/patches/v1.9/modules/nodejs/pre/runPreload.js");
+
+const createPostload = require("../NextLanguage-Source/patches/v1.9/modules/nodejs/post/createPostload.js");
+const runPostload = require("../NextLanguage-Source/patches/v1.9/modules/nodejs/post/runPostload.js");
 
 addOutput("Custom package is running.");
