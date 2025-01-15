f924faa473e6169064ef9022cda87a3fe3c08ddf
Authie2 <rblupdates.auth@gmail.com>
2025-01-15 20:29:48 +0700

    Updated settings

--- Plugin.js
+++ Plugin.js
@@ -3,17 +3,14 @@ function Service() {
     const Custom = package.create('package', path.join(__dirname, '../../../../Custom/default.js'), 'Custom');
 
     // Enables the plugin
-    Custom.config('package', 'Custom', 'enabled', true);
-
-    // Returns if the plugin is enabled or not
-    const enabled = Custom.strings.full.package.dev.self[1];
+    Custom.enabled = true;
 
     // Additional Plugin Information
-    Custom.config('package', 'Custom', 'author', 'Cassitydev');
-    Custom.config('package', 'Custom', 'version', '0.1.0');
-    Custom.config('package', 'Custom', 'repo', 'https://github.com/nxoscloud/Custom');
-    Custom.config('package', 'Custom', 'description', '(Custom package for NextLanguage) A Custom testing package for NextLanguages v2.0-dev plugin system');
-
+    Custom.author = 'Cassitydev';
+    Custom.version = '0.1.1';
+    Custom.repository = 'https://github.com/nxoscloud/Custom';
+    Custom.description = '(Custom package for NextLanguage) A Custom testing package for NextLanguages v2.0-dev plugin system';
+    
     // Optional Return value
     return Custom.strings
 }
